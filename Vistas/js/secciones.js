fc98eb760c45ecc50b5a0497bcb366dc4d786c95
JLeal5 <91571404+JLeal5@users.noreply.github.com>
2021-11-08 09:01:57 -0500

    Se añaden características de administración de aulas

--- Vistas/js/secciones.js
+++ Vistas/js/secciones.js
@@ -24,6 +24,31 @@ $(".box").on("click", ".AgregarArchivo", function(){
 })
 
 
+$(".box").on("click", ".BorrarSeccion", function(){
+
+    var Sid = $(this).attr("Sid");
+
+    var datos = new FormData();
+
+    datos.append("Sid", Sid);
+
+    $.ajax({
+
+        url: "http://localhost/Aulas/Ajax/seccionA.php",
+        method: "POST",
+        data: datos,
+        dataType: "json",
+        cache: false,
+        contentType: false,
+        processData: false,
+
+        success: function(resultado){
+
+            $("#idSE").val(resultado["id"]);
+        }
+    })
+})
+
 
 
 
