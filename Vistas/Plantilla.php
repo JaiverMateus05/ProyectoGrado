e83314b41805634e6a11e25d5a5d9996cd740b12
JaiverMateus <80165323+JaiverMateus05@users.noreply.github.com>
2022-11-17 19:50:14 -0500

    cambios prototipo 2 y 3

--- Vistas/Plantilla.php
+++ Vistas/Plantilla.php
@@ -5,62 +5,14 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<script type="text/javascript">
+<!-- <script type="text/javascript">
   document.oncontextmenu = function(){
   return false;
 }
-</script>
-
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Aula</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/dist/css/skins/_all-skins.min.css">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/morris.js/morris.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/jvectormap/jquery-jvectormap.css">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/bootstrap-daterangepicker/daterangepicker.css">
-
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/datatables.net-bs/css/responsive.bootstrap.min.css">
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-
-  <link rel="stylesheet" href="http://localhost/Aulas/Vistas/bower_components/select2/dist/css/select2.min.css">
+</script> -->
 
 
-
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-
-  <script src="http://localhost/Aulas/Vistas/sweetalert2/sweetalert2.all.js"></script>
-
-</head>
-
-<body class="hold-transition skin-blue sidebar-mini login-page">
+<body class="hold-transition skin-blue  sidebar-mini login-page">
 
 
   <div class="wrapper">
@@ -72,15 +24,15 @@ session_start();
 
       echo '<div class="wrapper">';
 
-      include "modulos/cabecera.php";
-
-      if ($_SESSION["rol"] == "Administrador") {
+/*      include "modulos/cabecera.php";
+ */
+     if ($_SESSION["rol"] == "Administrador") {
         include "modulos/menu.php";
       } else if ($_SESSION["rol"] == "Estudiante") {
         include "modulos/menuE.php";
       } else {
         include "modulos/menuP.php";
-      }
+      } 
 
 
       $url = array();
@@ -90,13 +42,11 @@ session_start();
         if (
           $url[0] == "Inicio" || $url[0] == "Salir" || $url[0] == "Mis-Datos" || $url[0] == "Usuarios" || $url[0] == "Carreras" || $url[0] == "Editar-Carrera"
           || $url[0] == "Estudiantes" || $url[0] == "Aulas" || $url[0] == "Aulas-Virtuales" || $url[0] == "Inscribir" || $url[0] == "Aula"
-          || $url[0] == "Mis-Aulas" || $url[0] == "Inscritos" || $url[0] == "D-S" || $url[0] == "Tarea" || $url[0] == "Entregas"
+          || $url[0] == "Mis-Aulas" || $url[0] == "Inscritos" || $url[0] == "D-S" || $url[0] == "Tarea" || $url[0] == "Entregas" || $url[0] == "Reportes" || $url[0] == "ReportesExcel"
         ) {
           include "modulos/" . $url[0] . ".php";
         }
-      } else {
-        include "modulos/Inicio.php";
-      }
+      } 
 
       echo '</div>';
     } else if (isset($_GET["url"])) {
@@ -180,6 +130,9 @@ session_start();
   <script src="http://localhost/Aulas/Vistas/js/aulas.js"></script>
 
   <script src="http://localhost/Aulas/Vistas/js/secciones.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
 
 </body>
 
